2c52ac70d9aec926d060891df2ef55eff41ee0be
Martina Kolar <martina.kolar7@gmail.com>
2023-07-30 16:40:08 +0200

    Rearranged the code a bit

--- downloader_main.py
+++ downloader_main.py
@@ -1,18 +1,18 @@
 from pytube import YouTube
 import getpass
 
+#download location
 username = getpass.getuser() 
 yt_videos_download_path = 'C:\\Users\\' + username + '\\Videos\\yt videos' 
 
+#link insertion
 link = input("Insert video link: ")
-
 yt = YouTube(link)
 
-print("Video ", yt.title, " is downloading.")
-
+#download
 yt_vid_download = yt.streams.get_highest_resolution()
 yt_vid_download.download(yt_videos_download_path)
 
-
+print("Video ", yt.title, " is being downloaded...")
 print("Video has been downloaded to the " + yt_videos_download_path + " folder. Enjoy! :)")
 
